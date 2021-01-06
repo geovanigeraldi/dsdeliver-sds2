754e5d0beca3e5e119697497c3ad06d0b85c3572
Geovani Geraldi <geovani.geraldi@gmail.com>
2021-01-05 22:19:47 -0300

    GET /products

---
+++ backend/src/main/java/com/geraldi/dsdeliver/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.geraldi.dsdeliver.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.geraldi.dsdeliver.DTO.ProductDTO;
+import com.geraldi.dsdeliver.entities.Product;
+import com.geraldi.dsdeliver.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repository;
+
+	@Transactional(readOnly = true)
+	public List<ProductDTO> findAll() {
+		List<Product> list = repository.findAllByOrderByNameAsc();
+		return list.stream().map(x -> new ProductDTO(x)).collect(Collectors.toList());
+	}
+
+}
